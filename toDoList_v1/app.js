c8a99cbc005b7b541c8a2d4aa18b70da42c956f8
Edward Ng <edward1477@gmail.com>
2021-06-24 22:09:51 +0800

    add css style

--- toDoList_v1/app.js
+++ toDoList_v1/app.js
@@ -5,10 +5,12 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
-var items = [];
+var items = ["Buy Food", "Cook Food", "Eat Food"];
 
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
 app.get("/", function (req, res) {
 
   var today = new Date();
