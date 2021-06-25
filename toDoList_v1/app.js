bbeb8d7c62a69ea1f058ed453ec9673711433484
Edward Ng <edward1477@gmail.com>
2021-06-25 16:01:02 +0800

    create date module

--- toDoList_v1/app.js
+++ toDoList_v1/app.js
@@ -2,10 +2,10 @@
 
 const express = require("express");
 const bodyParser = require("body-parser");
+const date = require(__dirname + "/date.js");
 
 const app = express();
-
-var items = ["Buy Food", "Cook Food", "Eat Food"];
+const items = ["Buy Food", "Cook Food", "Eat Food"];
 
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -13,15 +13,7 @@ app.use(express.static("public"));
 
 app.get("/", function (req, res) {
 
-  var today = new Date();
-  //var currentDay = today.getDay();
-  var options = {
-    weekday: "long",
-    day: "numeric",
-    month: "long"
-  };
-
-  var day = today.toLocaleDateString("en-US", options);
+  let day = date.getDate();
 
   res.render("list", { day: day, items: items });
 
@@ -34,7 +26,6 @@ app.post("/", function (req, res) {
   res.redirect("/");
 
 });
-
 
 app.listen(3000, function () {
   console.log("Server started on port 3000.");
