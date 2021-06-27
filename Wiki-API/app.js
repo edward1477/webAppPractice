61710ff0df188d348541c9848cf75f687df0882f
Edward Ng <edward1477@gmail.com>
2021-06-27 22:53:27 +0800

    create the API get request to the collections

--- Wiki-API/app.js
+++ Wiki-API/app.js
@@ -10,7 +10,33 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
+// MongoDB databse setup
+//1. Create a connection to the targeted DB: wikiDB
+mongoose.connect("mongodb://localhost:27017/wikiDB", { useNewUrlParser: true });
 
+//2. Create a DB schema variable (This is the collection of the connected DB)
+const articleSchema = {
+    title: String,
+    content: String
+};
+
+//3. Create a mongoose mode based on the schema created
+const Article = mongoose.model("Article", articleSchema);
+
+//RESTful implementations
+//1. GET request to the collections (articles), fetches ALL the articles
+app.get("/articles", function (req, res) {
+    Article.find({}, function (err, foundArticles) {
+        if (!err) {
+            //console.log(foundArticles); use this to test for our code in first glance
+            res.send(foundArticles);
+        } else {
+            res.send(err);
+        }
+    });
+});
+
+//Start and continue monitoring the port for any incoming request
 app.listen(3000, function () {
     console.log("Server started on port 3000.");
 });
