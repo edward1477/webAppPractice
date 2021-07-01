713b918f33111eba7457b2054af7a4263ba34d9b
Edward Ng <edward1477@gmail.com>
2021-07-01 22:53:57 +0800

    initialize home, login, register route get request

--- Secrets/app.js
+++ Secrets/app.js
@@ -9,6 +9,20 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
+app.get("/", function (req, res) {
+    res.render("home");
+});
+
+app.get("/login", function (req, res) {
+    res.render("login");
+});
+
+app.get("/register", function (req, res) {
+    res.render("register");
+});
+
+
+
 
 
 
