16d0e2244c81ca9961d5cc2aa905d649cf891480
Edward Ng <edward1477@gmail.com>
2021-07-09 21:14:31 +0800

    Level 4 hash and salt using bcrypt implementation

--- Secrets/app.js
+++ Secrets/app.js
@@ -9,9 +9,11 @@ const mongoose = require("mongoose");
 //Not use encryption when we turn to use Hash
 
 // Level 3: Hash function
-const md5 = require("md5");
+//const md5 = require("md5");
 
-
+// Level 4: Hash & Salt and using bcrypt npm module instead of MD5
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
 
 const app = express();
 
@@ -57,32 +59,42 @@ app.get("/register", function (req, res) {
 
 // Handle POST request of new user registration
 app.post("/register", function (req, res) {
-    const newUser = new User({
-        email: req.body.username,
-        password: md5(req.body.password)
-    });
-    newUser.save(function (err) {
-        if (err) {
-            console.log(err);
-        } else {
-            res.render("secrets");
-        }
+
+    bcrypt.hash(req.body.password, saltRounds, function (err, hash) {
+        // Store hash in your password DB.
+        const newUser = new User({
+            email: req.body.username,
+            password: hash
+        });
+        newUser.save(function (err) {
+            if (err) {
+                console.log(err);
+            } else {
+                res.render("secrets");
+            }
+        });
     });
 });
 
 // Handle POST request of existing user login
 app.post("/login", function (req, res) {
     const username = req.body.username;
-    const password = md5(req.body.password);
+    const password = req.body.password;
+    //const password = md5(req.body.password);
 
     User.findOne({ email: username }, function (err, foundUser) {
         if (err) {
             console.log(err);
         } else {
             if (foundUser) {
-                if (foundUser.password === password) {
-                    res.render("secrets");
-                }
+                bcrypt.compare(password, foundUser.password, function (err, result) {
+                    if (result == true) {
+                        res.render("secrets");
+                    }
+                });
+
+
+
             }
         }
     });
